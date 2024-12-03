15972ef38fd16a31049a60d9c8cbcf07b3cd7db3
Darling <rombk2711@gmail.com>
2024-12-04 04:13:25 +0700

    add auto logger for requests

--- CompressionService/src/main/java/com/shadowshiftstudio/compressionservice/config/SecurityConfig.java
+++ CompressionService/src/main/java/com/shadowshiftstudio/compressionservice/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
