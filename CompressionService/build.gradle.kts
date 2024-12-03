c33952b9fa625f4027afdc6dc83925dcb3ff6c12
Darling <rombk2711@gmail.com>
2024-12-04 03:28:20 +0700

    add configurations for app and security

--- CompressionService/build.gradle.kts
+++ CompressionService/build.gradle.kts
@@ -20,6 +20,19 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("net.coobird:thumbnailator:0.4.18")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.projectlombok:lombok")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	runtimeOnly("org.postgresql:postgresql")
+
+	testImplementation("org.mockito:mockito-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
