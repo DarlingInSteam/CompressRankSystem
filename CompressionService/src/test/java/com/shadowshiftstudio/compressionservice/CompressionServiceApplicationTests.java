3e910a1dc79520fe32829cc3bb6c3ac5fba0738d
Darling <rombk2711@gmail.com>
2024-12-04 01:33:52 +0700

    Initial commit

---
+++ CompressionService/src/test/java/com/shadowshiftstudio/compressionservice/CompressionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadowshiftstudio.compressionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
